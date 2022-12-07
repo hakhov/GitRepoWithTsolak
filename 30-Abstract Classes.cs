0d76f22f310e2b3c892d5cad28cd3f82022da335
Hakob Hovhannisyan <63061647+hakhov@users.noreply.github.com>
2022-12-07 22:55:28 +0400

    07.12.2022

---
+++ 30-Abstract Classes.cs
@@ -0,0 +1,24 @@
+﻿
+//https://www.hackerrank.com/challenges/30-abstract-classes/problem?isFullScreen=true
+
+class MyBook : Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Price { get; set; }
+
+    public MyBook(string title, string author, int price)
+    : base(title, author)
+    {
+        Title = title;
+        Author = author;
+        Price = price;
+    }
+
+    public override void display()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Price: {Price}");
+    }
+}
