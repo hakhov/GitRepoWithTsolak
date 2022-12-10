a8a8393f561d60597350c4708783c38289cee36c
Hakob Hovhannisyan <63061647+hakhov@users.noreply.github.com>
2022-12-10 21:44:10 +0400

    new implementation

--- 30-linkedList.cs
+++ 30-linkedList.cs
@@ -7,6 +7,47 @@ while (T-- > 0)
     linkedList = InsertElement(linkedList, data);
 }
 
+RemoveDuplicates(linkedList);
+
+Display(linkedList);
+
+static void Display(Node? linkedList)
+{
+    Node? start = linkedList;
+    while (start is not null)
+    {
+        Console.Write(start.Data + " ");
+        start = start.Next;
+    }
+}
+
+static Node InsertElement(Node? linkedList, int data)
+{
+    Node tempObj = new Node(data);
+
+    if (linkedList is null)
+        linkedList = tempObj;
+    else if (linkedList.Next is null)
+        linkedList.Next = tempObj;
+    else
+    {
+        Node start = linkedList;
+        while (start.Next is not null)
+            start = start.Next;
+        start.Next = tempObj;
+    }
+    return linkedList;
+}
+
+Node? linkedList = null;
+int T = Int32.Parse(Console.ReadLine());
+
+while (T-- > 0)
+{
+    int data = Int32.Parse(Console.ReadLine());
+    linkedList = InsertElement(linkedList, data);
+}
+
 RemoveDuplicates(ref linkedList);
 
 Display(linkedList);
@@ -39,22 +80,18 @@ static Node InsertElement(Node? linkedList, int data)
     return linkedList;
 }
 
-static Node? RemoveDuplicates(ref Node linkedList)
+static Node? RemoveDuplicates(Node linkedList)
 {
-    Node? tempObj = linkedList;
-
-    while (tempObj?.Next is not null)
+    var tempObj = linkedList;
+    while (tempObj != null && tempObj.Next != null)
     {
-        if (tempObj.Data == tempObj.Next.Data)
+        while (tempObj.Next != null && tempObj.Data == tempObj.Next.Data)
         {
             tempObj.Next = tempObj.Next.Next;
         }
-        else
-        {
-            tempObj = tempObj.Next;
-        }
+        tempObj = tempObj.Next;
     }
-    return tempObj;
+    return linkedList;
 }
 
 class Node
